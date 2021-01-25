4c332553f7dc403571d6535f3fda46c8363d2f3d
Mario Gallo <marioffgallo@hotmail.com>
2021-01-25 16:25:49 -0300

    Reestruturando pastas e adicionando Angular

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCO-thNUvBXucJZUGHvCEbl51ZmZp50sKQ",
+    authDomain: "padaria-baopao.firebaseapp.com",
+    databaseURL: "https://padaria-baopao-default-rtdb.firebaseio.com",
+    projectId: "padaria-baopao",
+    storageBucket: "padaria-baopao.appspot.com",
+    messagingSenderId: "1074457731623",
+    appId: "1:1074457731623:web:46ed4c76a576c8e0cfa402",
+    measurementId: "G-WVBX36928K"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCO-thNUvBXucJZUGHvCEbl51ZmZp50sKQ",
+    authDomain: "padaria-baopao.firebaseapp.com",
+    databaseURL: "https://padaria-baopao-default-rtdb.firebaseio.com",
+    projectId: "padaria-baopao",
+    storageBucket: "padaria-baopao.appspot.com",
+    messagingSenderId: "1074457731623",
+    appId: "1:1074457731623:web:46ed4c76a576c8e0cfa402",
+    measurementId: "G-WVBX36928K"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
